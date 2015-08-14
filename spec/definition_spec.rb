3a4a98f44c498d0d90cc76a665603866be6f8e9c
Aileen Cacayorin <acacayorin@gmail.com>
2015-08-14 11:04:54 -0700

    It returns the type of definition

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -3,10 +3,21 @@ require 'pry'
 require 'definition'
 
 describe Definition do
-    describe "#definition" do
-      it 'returns a definition' do
-        program = Definition.new({:definition => "A planned series of future events, items, or PErformance", :type => 'noun'})
-        expect(program.definition).to eq("a planned series of future events, items, or performance")
-      end
+  describe "#definition" do
+    it 'returns a definition' do
+      program = Definition.new({:definition => "A planned series of future events, items, or PErformance", :type => 'noun'})
+      expect(program.definition).to eq("a planned series of future events, items, or performance")
     end
+  end
+
+  describe "type" do
+    it 'returns the type of definition' do
+      program = Definition.new({:definition => "A planned series of future events, items, or PErformance", :type => 'noun'})
+      expect(program.type).to eq("noun")
+
+    end
+  end
+
+
+
 end
